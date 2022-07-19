59068ee163fd0777fffc04135e04fdfb55f294e0
Daniil <danykov97@mail.ru>
2022-07-19 23:24:03 +0300

    вторая итерация

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -63,5 +63,5 @@ class FollowSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         if data['user'] == data['following']:
-            raise serializers.ValidationError('Нельзя подписаться на себя' )
+            raise serializers.ValidationError('Нельзя подписаться на себя')
         return data
